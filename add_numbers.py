8f2e00848741d94b971a235ff4850147ecda86ba
Jane Pan <103807354+jjanepan@users.noreply.github.com>
2024-09-14 16:48:24 -0400

    Delete add_numbers.py

--- add_numbers.py
+++
@@ -1,8 +0,0 @@
-def add_two_numbers(a, b):
-    return a + b
-
-if __name__ == "__main__":
-    num1 = 5
-    num2 = 10
-    sum = add_two_numbers(num1, num2)
-    print(f"The sum of {num1} and {num2} is: {sum}")
